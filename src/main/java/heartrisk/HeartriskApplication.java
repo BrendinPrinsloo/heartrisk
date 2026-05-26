53a64321eb1c22aa001390685b2809a6ad1c5499
Brendin Prinsloo <brendinprinsloo@gmail.com>
2026-05-26 10:42:16 +0100

    Initial commit

--- src/main/java/heartrisk/HeartriskApplication.java
+++ src/main/java/heartrisk/HeartriskApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeartriskApplication {
+public class git initHeartriskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeartriskApplication.class, args);
