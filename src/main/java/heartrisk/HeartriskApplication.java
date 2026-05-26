d4f4733cb0800c2cfe4ed7e889b343396675b311
Brendin Prinsloo <brendinprinsloo@gmail.com>
2026-05-26 11:09:31 +0100

    Fix main application class

--- src/main/java/heartrisk/HeartriskApplication.java
+++ src/main/java/heartrisk/HeartriskApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class git initHeartriskApplication {
+public class HeartriskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeartriskApplication.class, args);
 	}
-
 }
